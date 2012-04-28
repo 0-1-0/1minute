cf9b11a34368c501ad410ef49e31c1817a84a6de
Roman Kolesnev <rvkolesnev@gmail.com>
2012-04-28 19:15:27 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oneminute::Application.config.secret_token = 'e805c28fe6f9ff7d1297ae57c25090f8d586d2469befd06bc30295a9e1395f2aad5c965ccdcf3fd4d0db1599721fd4f25aa5d7ed824af62fe2483664356defde'
