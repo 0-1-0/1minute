8ac041c73ec048cd9fbe39763fd05a55bc8e4d9b
Roman Kolesnev <rvkolesnev@gmail.com>
2012-05-07 09:23:56 +0400

    lll

--- db/migrate/20120506183741_add_many_things_to_events.rb
+++ db/migrate/20120506183741_add_many_things_to_events.rb
@@ -1,6 +1,6 @@
 class AddManyThingsToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :conv_course, :float
+    add_column :events, :conv_course, :float, default: 1.0
     add_column :events, :ex_name, :string
   end
 end
