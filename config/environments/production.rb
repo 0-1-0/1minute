3947e11c0bb36385f2a634eba9a636ab66e3fdd6
Nickolay Yegorov <nickolay@yegorov.info>
2012-06-19 00:32:21 +0400

    fixed bootstrap css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Oneminute::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w( bootstrap.min.css )
 
   # Generate digests for assets URLs
   config.assets.digest = true
